124feb775a967771e217ecf39a74ab52a63b126e
Ефим Бульен <143902970+EfimBulien@users.noreply.github.com>
2024-09-11 15:46:07 +0300

    tests

---
+++ PasswordCheck.Tests/PassCheckTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PasswordCheck.Tests
+{
+    [TestClass]
+    public class PassCheckTests
+    {
+        [TestMethod]
+        public void Pass_passwordcheck_0return()
+        {
+            const int expected = 0;
+            const string password = "пароль";
+            var passCheck = new PassCheck();
+            var actual = passCheck.Pass(password);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Pass_passwordcheck_1return()
+        {
+            const int expected = 1;
+            const string password = "password";
+            var passCheck = new PassCheck();
+            var actual = passCheck.Pass(password);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Pass_passwordcheck_2return()
+        {
+            const int expected = 2;
+            const string password = "password1";
+            var passCheck = new PassCheck();
+            var actual = passCheck.Pass(password);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Pass_passwordcheck_3return()
+        {
+            const int expected = 3;
+            const string password = "Password1";
+            var passCheck = new PassCheck();
+            var actual = passCheck.Pass(password);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Pass_passwordcheck_4return()
+        {
+            const int expected = 4;
+            const string password = "Pass|word1";
+            var passCheck = new PassCheck();
+            var actual = passCheck.Pass(password);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Pass_passwordcheck_5return()
+        {
+            const int expected = 5;
+            const string password = "Pass|word10";
+            var passCheck = new PassCheck();
+            var actual = passCheck.Pass(password);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ PasswordCheck/PassCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PasswordCheck
+{
+    public class PassCheck
+    {
+        public int Pass(string password)
+        {
+            var passBall = 0;
+            if (password.Length > 10) {
+                passBall++;
+            }
+            if (Regex.IsMatch(password, "[A-Z]")) {
+                passBall++;
+            }
+            if (Regex.IsMatch(password, "[a-z]")) 
+            
+            {
+                passBall++;
+            }
+            if (Regex.IsMatch(password, "\\d"))
+            {
+                passBall++;
+            }
+            if(Regex.IsMatch(password, "[!@#$%^&*(),.?\":{}|<>]"))
+            {
+                passBall++;
+            }
+            return passBall;
+        }
+    }
+}
